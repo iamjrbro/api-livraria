8eaef17bf4b201fc75d617321508cb284cd32f6f
Julia Ribeiro <jrstudin@gmail.com>
2023-10-15 13:47:40 -0300

    added srv

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -3,7 +3,7 @@ import mongoose, {mongo} from "mongoose";
 //import mongoose, {mongo} from "mongoose"
 
 async function conectaDB() {
-    mongoose.connect("mongodb+srv:admin:<admin123>@cluster0.6yw9lcl.mongodb.net/library?retryWrites=true&w=majority");
+    mongoose.connect("mongodb+srv://admin:admin123@cluster0.6yw9lcl.mongodb.net/livraria?retryWrites=true&w=majority");
     return mongoose.connection;
 }
 
