92b186205aac320c94b17cf3114edefc7cabb165
Julia Ribeiro <112039292+iamjrbro@users.noreply.github.com>
2023-10-24 12:50:56 -0300

    Update autoresController.js
    updated autoresController

--- src/contollers/autoresController.js
+++ src/contollers/autoresController.js
@@ -25,12 +25,23 @@ class AutorController{
         };
     };
 
-    static async atualizarAutor(req, res){
+
+    static async cadastrarAutor(req, res){
         try{
             const novoAutor = await autor.create(req.body);
             res.status(201).json({message: "Autor cadastrado com sucesso!", autor: novoAutor});
         } catch(error){
             res.status(500).json({message: `${error.message} - FALHA AO CADASTRAR AUTOR`});
+        };
+    };
+
+    static async atualizarAutor(req, res){
+        try{
+            const id = req.params.id;
+            await autor.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message:"Autor atualizado!"})
+        } catch(error){
+            res.status(500).json({message:`${error.message} - FALHA AO ATUALIZAR AUTOR`})
         };
     };
 
@@ -44,3 +55,6 @@ class AutorController{
         };
     };
 }
+
+
+export {AutorController};
