3430435f240a559a17cb9259108d35657a5676cb
Julia Ribeiro <jrstudin@gmail.com>
2023-10-28 19:52:06 -0300

    .

--- src/contollers/editoraController.js
+++ src/contollers/editoraController.js
@@ -1,20 +1,5 @@
-<<<<<<< HEAD
-import { autor } from "../models/Autor.js";
-import livroModel from "../models/Livro.js";  
-
-class editoraController{
-    static async livroPorEditora(req, res) {
-        const editora = req.query.editora;
-        try {
-        const livrosEditora = await livroModel.find({ editora: editora });
-        res.status(200).json(livrosEditora);
-} catch (error) {
-    res.status(500).json({ message: "FALHA NA BUSCA" });
-    };
-};
-};
-=======
 import { editora } from "../models/Editora";
+import { autor } from "../models/Autor";
 import livroModel from "../models/Livro";
 
 class editoraController{
@@ -30,4 +15,20 @@ class editoraController{
 };
 
 export default editoraController;
->>>>>>> ef4c7c75302f4f942b0e296d897611fd22fabc60
+
+
+
+/*import { autor } from "../models/Autor.js";
+import livroModel from "../models/Livro.js";  
+
+class editoraController{
+    static async livroPorEditora(req, res) {
+        const editora = req.query.editora;
+        try {
+        const livrosEditora = await livroModel.find({ editora: editora });
+        res.status(200).json(livrosEditora);
+} catch (error) {
+    res.status(500).json({ message: "FALHA NA BUSCA" });
+    };
+};
+};*/
